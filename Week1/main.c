bc728bdd5feca4a626b567be5547209e8c4ead30
Cala Dausas <226167917@nust.na>
2026-09-24 17:18:08 +0200

    Week 1 - First C Program

---
+++ Week1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    char municipality[50];
+    char mayor[50];
+    int population;
+
+    printf("Municipal Financial Management System\n");
+    printf("Welcome to Windhoek Municipality\n");
+
+    printf("Enter Municipality Name: ");
+    scanf("%49s", municipality);
+
+    printf("Enter Mayor's Name: ");
+    scanf("%49s", mayor);
+
+    printf("Enter Population: ");
+    scanf("%d", &population);
+
+
+    printf("Municipality: %s\n", municipality);
+    printf("Mayor: %s\n", mayor);
+    printf("Population: %d\n", population);
+
+    return 0;
+}
